2f5e757dd632279ec41fc0e4afe27d00e8fb8e23
unverzed <vitoriarortega@gmail.com>
2022-08-14 15:25:09 -0300

    feat: render avaliations from all users

--- src/repositories/avaliationsRepository.ts
+++ src/repositories/avaliationsRepository.ts
@@ -1,19 +1,29 @@
 import prisma from "../config/database";
 
-export async function insert(avaliation: any, user_id: number, game_id: number) {
-  await prisma.avaliations.create({ data: { ...avaliation, user_id, game_id } });
+export async function insert(
+  avaliation: any,
+  user_id: number,
+  game_id: number
+) {
+  await prisma.avaliations.create({
+    data: { ...avaliation, user_id, game_id },
+  });
 }
 
-export async function getAllAvaliations(id: number){
-  return await prisma.avaliations.findMany({where: {game_id: id}});
+export async function getAllAvaliations(id: number) {
+  const idNumber = id;
+  return await prisma.$queryRaw`SELECT * from avaliations
+  JOIN games AS g ON game_id = g.id
+  JOIN users AS u ON avaliations.user_id = u.id
+  WHERE game_id = ${idNumber}
+  ORDER BY avaliations.created_at DESC`;
 }
-
 
 export async function deleteAvaliation(id: number, game_id: number) {
   return await prisma.avaliations.delete({
     where: {
       id: id,
-    }, 
+    },
   });
 }
 const avaliationsRepository = { insert, deleteAvaliation, getAllAvaliations };
